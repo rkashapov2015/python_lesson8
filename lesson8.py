1db2b9aa62296e458983d4440a675cc2e7f25eee
Rinat Kashapov <rinatkzz@yandex.ru>
2018-09-19 22:45:02 +0300

    first commit

---
+++ lesson8.py
@@ -0,0 +1,12 @@
+import os
+
+migrations = 'Migrations'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+migration_path = os.path.join(current_dir, migrations)
+
+
+
+#if __name__ == '__main__':
+#    print(__name__)
+#    pass
