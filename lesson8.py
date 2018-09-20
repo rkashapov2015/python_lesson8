0b04c9f634f04c098f4926455abee94f1ffb4c77
Rinat Kashapov <rinatkzz@yandex.ru>
2018-09-20 21:47:04 +0300

    working

--- lesson8.py
+++ lesson8.py
@@ -1,12 +1,39 @@
 import os
+import mmap
 
 migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 migration_path = os.path.join(current_dir, migrations)
 
+keepGoing = True
 
+def find_from_file(file_path, needed_word):
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if needed_word in line:
+                    return True
+        return False
+    except UnicodeError:
+        return False
 
-#if __name__ == '__main__':
-#    print(__name__)
-#    pass
+print(f"Абсолютный путь: {migration_path}")
+
+while (keepGoing):
+    try: 
+        needed_words = input('Enter needed word: ')
+        if not needed_words:
+            keepGoing = False
+            continue
+
+        list_files = os.listdir(migration_path)
+        number_files = 0
+        for file_name in list_files:
+            file_path = os.path.join(migration_path, file_name)
+            if find_from_file(file_path, needed_words):
+                print(file_name)
+                number_files += 1
+        print(f"Количество файлов {number_files}")
+    except KeyboardInterrupt:
+        keepGoing = False
